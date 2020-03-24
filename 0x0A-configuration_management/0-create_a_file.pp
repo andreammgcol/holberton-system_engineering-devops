2b5eeccf0f720d5b913e045a978648cedc6578db
andreammgcol <amendez72@misena.edu.co>
2020-03-23 20:12:02 -0500

    fixing indentation error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Using Puppet, create a file in /tmp
 file { 'holberton':
-    path => '/tmp/holberton',
-    owner => 'www-data',
-    group => 'www-data',
-    mode  => '0744',
-    content => 'I love Puppet'
-} 
+  path    => '/tmp/holberton',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+}
